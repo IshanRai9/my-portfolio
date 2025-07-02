28468f626ebc930d889f0cbea301f6b12a139860
IshanRai9 <raiishan0911@gmail.com>
2025-07-03 01:41:13 +0530

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import '../styles/globals.css'; // path change after moving CSS
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
